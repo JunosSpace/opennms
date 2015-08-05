969a48382b006815ae1bdc2cc77cfff0342fab36
Yonghua Zhan <yhzhan@juniper.net>
2015-08-04 18:43:35 -0700

    JUNIPER-ONLY: PR975528

--- opennms-web-api/src/main/java/org/opennms/web/api/Util.java
+++ opennms-web-api/src/main/java/org/opennms/web/api/Util.java
@@ -169,7 +169,7 @@ public abstract class Util extends Object {
         for (int i = 0; i < hostHeaders.length; ++i) {
         	final String ret = request.getHeader(hostHeaders[i]);
             if (ret != null) {
-                return ret;
+                return ret.replaceAll("<", "&lt;").replaceAll(">", "&gt;").replaceAll("\"", "&quot;");
             }
         }
         return request.getServerName() + ":" + Integer.toString(request.getServerPort());
